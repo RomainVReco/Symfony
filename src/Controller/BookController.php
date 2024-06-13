984feaa7045490b013d059764608ad7853f75a65
Romain VORLET <romain.vorlet@gmail.com>
2024-06-13 22:59:32 +0200

    2ème route

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,18 +2,31 @@
 
 namespace App\Controller;
 
+use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class BookController extends AbstractController
 {
-    #[Route('/api/book', name:'book', methods:['GET'])]
-    public function getBookList(): JsonResponse
+    #[Route('/api/books', name:'book', methods:['GET'])]
+    public function getAllBooks(BookRepository $bookRepository, SerializerInterface $serializer): JsonResponse
     {
-        return new JsonResponse([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/BookController.php',
-        ]);
+        $bookList = $bookRepository->findAll();
+
+        $jsonBookList = $serializer->serialize($bookList, 'json');
+        return new JsonResponse($jsonBookList, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/books/{id}', name:'detailBook', methods:['GET'])]
+    public function getDetailBook(int $id, SerializerInterface $serializer, BookRepository $bookRepository) {
+        $book = $bookRepository->find($id);
+        if($book) {
+            $jsonBook = $serializer->serialize($book, 'json');
+            return new JsonResponse($jsonBook, Response::HTTP_OK, [], true);
+        }
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
     }
 }
