93dd2faa81dfaa379c92f65075609df1f6bc0fe0
Romain VORLET <romain.vorlet@gmail.com>
2024-06-16 21:30:41 +0200

    ajout create

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -7,8 +7,10 @@ use App\Entity\Book;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class BookController extends AbstractController
@@ -43,4 +45,18 @@ class BookController extends AbstractController
         }
         return new JsonResponse(null, Response::HTTP_NOT_FOUND);
     }    
+
+    #[Route('/api/books', name:"createBook", methods:['POST'])]
+    public function createBook (Request $request, SerializerInterface $serializer, 
+    EntityManagerInterface $em, UrlGeneratorInterface $urlGenerator): JsonResponse
+    {
+        $book = $serializer->deserialize($request->getContent(), Book::class, 'json');
+        $em->persist($book);
+        $em->flush();
+
+        $jsonBook = $serializer->serialize($book, 'json', ['groups' => 'getBooks']);
+        $location = $urlGenerator->generate('detailBook', ['id' => $book->getId()], UrlGeneratorInterface::ABSOLUTE_PATH);
+
+        return new JsonResponse($jsonBook, Response::HTTP_CREATED, ["Location" => $location], true);
+    }
 }
