2d5b7fcd1a96b0024131a9ac6f03cf7350382d7b
Romain VORLET <romain.vorlet@gmail.com>
2024-06-18 23:49:55 +0200

    ajout user

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,19 +6,31 @@ use App\Entity\Author;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Book;
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private $userPasswordHaser;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHaser)
+    {
+        $this->userPasswordHaser = $userPasswordHaser;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // for ($i = 0; $i < 10; $i++) {
-        //     $livre = new Book;
-        //     $livre->setTitle('Livre ' . $i);
-        //     $livre->setCoverText('Quatrième de couverture numéro : ' . $i);
-        //     $manager->persist($livre);
-        // }
+        $user = new User();
+        $user->setEmail("user@bookapi.com");
+        $user->setRoles(["ROLE_USER"]);
+        $user->setPassword($this->userPasswordHaser->hashPassword($user, 'password'));
+        $manager->persist($user);
 
-        // $manager->flush();
+        $userAdmin = new User();
+        $user->setEmail("admin@bookapi.com");
+        $user->setRoles(["ROLE_ADMIN"]);
+        $user->setPassword($this->userPasswordHaser->hashPassword($user, 'password'));
+        $manager->persist($userAdmin);
 
         $listAuthor = [];
         for ($i = 0; $i < 10; $i++) {
@@ -29,13 +41,14 @@ class AppFixtures extends Fixture
             $listAuthor[] = $author;
         }
 
-            for ($i = 0; $i < 20; $i++) {
+        for ($i = 0; $i < 20; $i++) {
             $livre = new Book;
             $livre->setTitle('Livre ' . $i);
             $livre->setCoverText('Quatrième de couverture numéro : ' . $i);
             $livre->setAuthor($listAuthor[array_rand($listAuthor)]);
             $manager->persist($livre);
         }
+
         $manager->flush();
     }
 }
